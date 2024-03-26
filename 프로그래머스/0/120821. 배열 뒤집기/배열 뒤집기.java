e0696c112d11766cdd0f47a9b4af524f15cfa7b8
DK <52989474+DK2554@users.noreply.github.com>
2024-03-26 22:31:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = Arrays.stream(num_list)
-                .boxed() // 기본 타입 -> 래퍼 타입
-                .collect(Collectors.toList());
-        Collections.reverse(list);
-        return list.stream().mapToInt(i->i).toArray();
+        int[] answer = new int[num_list.length];
+        for(int i = 0; i< num_list.length; i++){
+            answer[i] = num_list[num_list.length-i-1];
+        }
+        return answer;
     }
 }
