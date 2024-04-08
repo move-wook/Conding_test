586b8a041a30151b47a68d2628b3e74ea1323d52
DK <52989474+DK2554@users.noreply.github.com>
2024-04-09 04:17:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,9 +3,16 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] numbers) {
-
+        //간단하게 총합에서 배열에 있는 수를 빼면 간단함 ... 역시 논리적으로 고여버리면 끝나네
+        int sum = 45;
+        for (int i : numbers) {
+            sum -= i;
+        }
+        return sum;
+        //스트림으로 처리한 코드는 시간이 오래 걸림
+        /*
         return IntStream.rangeClosed(0, 9)
                         .filter(item -> Arrays.stream(numbers).noneMatch(number -> number == item))
-            .sum();
+            .sum();*/
     }
 }
