ab48e54f38187f3dfe72c15353c71dbe220c7db6
DK <52989474+DK2554@users.noreply.github.com>
2024-04-18 13:35:58 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -4,6 +4,10 @@ class Solution {
         for(int i=0; i < a.length; i++){
             answer += a[i] * b[i];
         }
+        //스트림을 활용한 풀이방법 
+        //IntStream 을 활용해서 a의 길이만큼 반복하면서 map 을통해 중간연산 작업을 거친 총합을 구하는 메서드를 호출해서 처리
+        //IntStream.range(0, a.length).map(index -> a[index] * b[index]).sum(); 
+        
         return answer;
     }
 }
