3e67c986bf5434bc8916ced1fa4813f76655287a
Lim Dong Uk <52989474+move-wook@users.noreply.github.com>
2025-03-21 11:16:50 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+public class Main {
+	public void solution() throws Exception{
+		StringBuilder std = new StringBuilder();
+		std.append("         ,r\'\"7\n")
+		.append("r`-_   ,'  ,/\n")
+		.append(" \\. \". L_r'\n")
+		.append("   `~\\/\n")
+		.append("      |\n")
+		.append("      |");
+		System.out.println(std.toString());
+
+	}
+	public static void main(String[] args) throws Exception {
+		
+		new Main().solution();
+	}
+}
