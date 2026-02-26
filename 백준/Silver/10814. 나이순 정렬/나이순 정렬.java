ebf6616f72dee02a1b968b4f4cc0b73b7e13e89d
Lim Dong Uk <52989474+move-wook@users.noreply.github.com>
2026-02-26 21:02:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 664 ms, Memory: 51492 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        
+        String[][] members = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            members[i][0] = st.nextToken(); // 나이
+            members[i][1] = st.nextToken(); // 이름
+        }
+
+        
+        Arrays.sort(members, (m1, m2) -> {
+          
+            return Integer.parseInt(m1[0]) - Integer.parseInt(m2[0]);
+        });
+
+       
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(members[i][0]).append(" ").append(members[i][1]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
